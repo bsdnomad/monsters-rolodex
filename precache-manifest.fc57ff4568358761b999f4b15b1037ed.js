ea8369e7709132d386ed021055e89030b8314556
Kliment Ognianov <kliment.ognianov@gmail.com>
2020-12-14 00:32:51 +0200

    Updates

---
+++ precache-manifest.fc57ff4568358761b999f4b15b1037ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6168d0f8548eac56a2eb54ab7825a29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "181437787989352095b4",
+    "url": "/monsters-rolodex/static/css/main.3897edea.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181437787989352095b4",
+    "url": "/monsters-rolodex/static/js/main.c810327b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
